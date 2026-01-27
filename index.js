f086c78f027ef66171e64c6900c6f1f58cc9b25c
Meat Puppets <leonardo_8.8@live.com>
2026-01-27 10:26:14 -0500

    Prueba con CI fallando

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send("Integracion continua trabajando")
-})
+    res.send("Integracion continua trabajando");
+});
 
 app.listen(port, () => {
     console.log(`Servidor trabajando en el puerto ${port}`);
-})
+});
