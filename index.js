1b576cb46ce294ef3941ff1c2518172ecd517153
Meat Puppets <leonardo_8.8@live.com>
2026-01-27 10:09:14 -0500

    Proyecto base con CI

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Integracion continua trabajando');
+})
+
+app.listen(port, () => {
+    console.log(`Servidor trabajando en el puerto ${port}`);
+})
